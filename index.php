73d87f44ae6b1cf3765f6f2750a9ee1faadca5c2
patrick mary <laube@hotmail.fr>
2012-06-01 16:00:10 +0200

    debut du fonctionnement en ajax

--- index.php
+++ index.php
@@ -1,34 +1,109 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script src="js/initXHR.js"></script>
-
-<script>
-function request() {
-	var xhr = getXMLHttpRequest();
-	
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-			  var doc = eval('(' + xhr.responseText + ')'); 
-                          var prix = doc.prix;
-                          $('span').html(prix +"&euro;");                        
-                          //var test = $("p").html();
-                          
-		}
-	};
-	
-	xhr.open("GET", "serverprocess.php", true);
-	xhr.send(null);
-}
-</script>
 <?php
-print '<script>';
+/* recuration data coté serveur */
+if(isset($_GET['type'])){ // 0=boisson 1=entree 2=plat
+    $type = $_GET['type'];
+    $col=array();
+    if($type==2){
+        $plat->id=45;
+        $plat->name = "steack frites";
+        $plat->description = "le super steak frites !!";
+        $plat->prix = 10;
+        $col[0] = $plat;
+        $plat = null;
+        $plat->id=85;
+        $plat->name = "poisson panne";
+        $plat->description = "le poisson panne !!";
+        $plat->prix = 15;
+        $col[1] = $plat;
+    }
+    if($type==0){
+        $plat->id=20;
+        $plat->name = "coca cola";
+        $plat->description = "coca cola !";
+        $plat->prix = 5;
+        $col[0] = $plat;
+    }
+    if($type==1){
+        $plat->id=5;
+        $plat->name = "salade composé";
+        $plat->description = "salade composé !!";
+        $plat->prix = 10;
+        $col[0] = $plat;
+    }
+    header('Content-type: application/json');
+    print json_encode($col);
+    exit;
+}
+
+print'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>';
+print'<script src="js/initXHR.js"></script>';
+
+/* requete ajax + traitement des données */
+print'<script>
+    tab = new Array();
+    function test(id){
+        var test = tab[id];
+        if(!test){
+            tab[id] = new Array("repas",1);
+            $(".recap").after("<p>"+tab[id][0]+"<span id=\""+id+"\">"+tab[id][1]+"</span></p>");
+           // $("#quantite").append(tab[id][1]);
+        }
+        else{
+            tab[id][1] = tab[id][1]+1;
+            $("#"+id).text(tab[id][1]);
+            
+        }
+    }
     
-print '</script>';
+    function requestfood(type) {
+            var xhr = getXMLHttpRequest();
 
-print 'hello world !';
+            xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+                            var doc = eval("(" + xhr.responseText + ")");
+                            $(".choix span").html("");
+                            for(var i= 0; i<doc.length; i++){
+                                object = doc[i];
+                                var id = doc[i].id;
+                                var name = doc[i].name;
+                                $(".choix span").append(
+                                "<input onClick=\"test("+id+")\" type=\"button\" value=\""+doc[i].name+"\">");
+                            }
+                            //var name = doc[0].name;
+                            //id="789";
+                            
 
-print '<input type="button" onClick="request();" value="steak frite"> ';
+                    }
+            };
 
-print 'Quantite : 0';
+            xhr.open("GET", "index.php?type="+type, true);
+            xhr.send(null);
+    }
+</script>';
 
-print '<p>Montant : <span></span></p>';
+/*print'<script>';
+    print'$(document).ready(function() {';
+        print'$(":button").click(function(){
+            alert("bouton clicke"); 
+            });';
+   print'});';     
+print'</script>';*/
+
+/* View */
+print'<div class="food">';
+    print'<ul>';
+        print'<li><a href="javascript:void(0);" onClick="requestfood(0);">Boisson</a></li>';
+        print'<li><a href="javascript:void(0);" onClick="requestfood(1);">Entree</a></li>';
+        print'<li><a href="javascript:void(0);" onClick="requestfood(2);">Plat Principal</a></li>';
+   print'</ul>';
+print'</div>';   
+
+print'<div class="choix">';
+    print '<span></span>';
+print'</div>';
+
+print'<div class="recap">';
+print'</div>';
+
+
 ?>
